848486d27e918d83dc1f044a7d0c42b87b3522eb
Amby <noreply@ambyce.eu>
2026-03-24 14:48:26 +0100

    Use record instead of old object declaration

--- src/main/java/eu/ambyce/Intersection.java
+++ src/main/java/eu/ambyce/Intersection.java
@@ -1,19 +1,3 @@
 package eu.ambyce;
 
-public class Intersection {
-    private final int id;
-    private final String name;
-
-    public Intersection(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-}
+public record Intersection(int id, String name) {}
